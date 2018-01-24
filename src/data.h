443b76ea442c553decf471e4bd372b5f5d972cc2
Alex Bowen <ajbowen249@gmail.com>
2018-01-23 19:26:33 -0500

    pull sprite color from final byte

--- src/data.h
+++ src/data.h
@@ -1,4 +1,3 @@
 extern const char intro[];
-extern const char spriteColors[];
 
 #define SPRITESHEET 0x2000
--- src/main.c
+++ src/main.c
@@ -15,12 +15,13 @@ void assignSprite(unsigned int sheetIndex, unsigned int vicIndex, char stretchX,
     char maskInv = ~mask;
     char stretchXByte = (stretchX ? 0x01 : 0x00) << vicIndex;
     char stretchYByte = (stretchY ? 0x01 : 0x00) << vicIndex;
+    char* spriteAddress = (char*)(SPRITESHEET + (sheetIndex * 0x40));
 
     spritePointer += vicIndex;
-    *spritePointer = (SPRITESHEET + (sheetIndex * 0x40)) / 0x40;
+    *spritePointer =  (SPRITESHEET + (sheetIndex * 0x40)) / 0x40;
 
     VIC.spr_ena = VIC.spr_ena | mask;
-    VIC.spr0_color = spriteColors[sheetIndex];
+    VIC.spr0_color = spriteAddress[63];
     VIC.spr_exp_x = (VIC.spr_exp_x & maskInv) | stretchXByte;
     VIC.spr_exp_y = (VIC.spr_exp_y & maskInv) | stretchYByte;
 }
